e7544bb7d8b28ac8f9ea1e23b21d3e869967917c
mishrashaksham <xaverian.4175@gmail.com>
2026-03-28 22:44:49 +0530

    Added createAccount function logic

--- src/accounts.c
+++ src/accounts.c
@@ -1,3 +1,31 @@
-//Shaksham
+// Shaksham
 
 #include "../include/structure.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateUID(char uid[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        uid[i] = 'A' + (rand() % 26);
+    }
+    for (int i = 4; i < 10; i++)
+    {
+        uid[i] = '0' + (rand() % 10);
+    }
+
+    // Phase 3: String ko properly close karna (Full stop)
+    uid[10] = '\0';
+}
+
+int main()
+{
+    srand(time(NULL));
+    char meraNayaID[11];
+    generateUID(meraNayaID);
+    printf("Success! Tera VirtuBank UID hai: %s\n", meraNayaID);
+    return 0;
+}
