937b83b6a1555379035fa883e2e68ae44bcff84d
praassuu <preee1220@gmail.com>
2026-03-29 18:02:45 +0530

    phase 1

--- src/main.c
+++ src/main.c
@@ -1,3 +1,51 @@
 //Prasansa
 
 #include "../include/structure.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int choice;
+    char uid[11];
+    int mpin;
+
+    
+    while(choice != 3)
+    {
+        printf("\n------- VIRTU BANK -------\n");
+        printf("1. Open Account\n");
+        printf("2. Login\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) 
+        {
+            case 1:
+            printf("Selected account is openned\n");
+            break;
+
+            case 2:
+            printf("Enter uid: ");
+            scanf("%s", uid);
+
+            printf("Enter mpin: ");
+            scanf("%d", &mpin);
+            break;
+
+            case 3:
+            printf("Successfully exited\n");
+            return 0;
+
+            default:
+            printf("Invalid choice\n re-enter \n");
+        }
+    }
+    return 0;
+}
+
+
+        
+
+        
